1970296180de3a0ced71c7ccd836a875bcf4d6fc
Max Kholostenko <drag817@yandex.ru>
2025-01-08 19:14:53 +0300

    app availability fix

--- app.py
+++ app.py
@@ -632,4 +632,4 @@ if __name__ == '__main__':
         if not os.path.exists(app.config['UPLOAD_FOLDER']):
             os.makedirs(app.config['UPLOAD_FOLDER'])
 
-    app.run(debug=True, port=3000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
